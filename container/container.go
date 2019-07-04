0fabddcc117aefb9e568e25e7937cebe12b10c59
lnquy065 <lnquy065@gmail.com>
2019-07-04 22:49:17 +0700

    Add exam container

--- container/container.go
+++ container/container.go
@@ -1,16 +1,21 @@
 package container
 
 import (
-	"github.com/google/wire"
 	"ts-server/apis/v1"
 	"ts-server/repositories"
 	"ts-server/services"
 )
 
 func CreateQuestionController() *apis.QuestionController {
-	panic(wire.Build(
-		repositories.NewQuestionRepository,
-		services.NewQuestionService,
-		apis.NewQuestionController))
-	return &apis.QuestionController{}
+	questionRepository := repositories.NewQuestionRepository()
+	questionService := services.NewQuestionService(questionRepository)
+	questionController := apis.NewQuestionController(questionService)
+	return questionController
+}
+
+func CreateExamController() *apis.ExamController {
+	examRepository := repositories.NewExamRepository()
+	examService := services.NewExamService(examRepository)
+	examController := apis.NewExamController(examService)
+	return examController
 }
