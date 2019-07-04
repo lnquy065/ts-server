caadc3963fd388de26bd7d1bdab42f35c851b898
lnquy065 <lnquy065@gmail.com>
2019-07-04 22:50:22 +0700

    Add endpoints for exam

--- router.go
+++ router.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"ts-server/container"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 
 	// Controllers
-	questionCtrl := CreateQuestionController()
+	questionCtrl := container.CreateQuestionController()
+	examCtrl := container.CreateExamController()
 
 	apiV1 := router.Group("v1")
 	{
@@ -16,6 +18,18 @@ func NewRouter() *gin.Engine {
 		{
 			questions.GET("/:index", questionCtrl.GetByIndex)
 			questions.GET("/", questionCtrl.GetAll)
+		}
+
+		exams := apiV1.Group("exams-non-questions")
+		{
+			exams.GET("/:id", examCtrl.GetById)
+			exams.GET("/", examCtrl.GetAll)
+		}
+
+		examsWithQ := apiV1.Group("exams-inc-questions")
+		{
+			examsWithQ.GET("/:id", examCtrl.GetByIdWithQuestions)
+			examsWithQ.GET("/", examCtrl.GetAllWithQuestions)
 		}
 	}
 	return router
