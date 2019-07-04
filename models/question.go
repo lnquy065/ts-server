4fe963a89b53b880b972f6ff1301a305de24f617
lnquy065 <lnquy065@gmail.com>
2019-07-04 21:40:22 +0700

    Add question_type

--- models/question.go
+++ models/question.go
@@ -1,17 +1,17 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 type Question struct {
 	gorm.Model
-	Question    string
-	QuestionImg string
-	LicenseType string
-	Index       int
-	Answers     []Answer `gorm:"foreignkey:QuestionId"`
-}
-
-type QuestionDTO struct {
+	Question     string
+	QuestionImg  string
+	QuestionType string
+	LicenseType  string
+	Index        int
+	Answers      []Answer `gorm:"foreignkey:QuestionId"`
 }
 
 func (this *Question) TableName() string {
